eff39cae59e6c9612df406f0ff178a7eb3cc5859
dawsonhammdev <hammsdesigns@gmail.com>
2020-07-23 08:04:07 -0500

    single number is done

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,28 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # we need to return the number that doesn't show up twice.
+    # we could use a for loop to iterate through the array but need 
+    # for x in arr:
+    #     if x != x:
+    #         print(x)
+    # a way to make sure there isn't a duplicate.
 
-    pass
+    # a set in python can have no duplicates.
+    # so if we compare the len of a set and the original list we should 
+    # be able to find the duplicate.
+    # if arr.Count(arr[0]) == len(set(arr)):
+    #     return True
+    # else:
+    #     return False
+
+    ans = arr[0]
+    for i in range(1,len(arr)):
+        ans ^= arr[i]
+    return ans
+
+
+    
 
 
 if __name__ == '__main__':
