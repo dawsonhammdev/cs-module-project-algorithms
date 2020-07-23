cd685e936fdd1be4ae6cd77a4207430d65414233
dawsonhammdev <hammsdesigns@gmail.com>
2020-07-23 16:03:20 -0500

    updates

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,20 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache):
     # Your code here
-
-    pass
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    # check the cache to see if it holds the answer this branch is looking for 
+    elif n in cache:
+        return cache[n]
+    else:
+        # otherwise, n is not in the cache, so we need to compute the answer the "normal" way
+        # once the answer is computed, save it in the cache 
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
